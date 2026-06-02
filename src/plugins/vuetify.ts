e9110ab5c93be0ba03cb40b3ca66ff79a8c8b931
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-06-02 11:26:21 +0300

    feat: set moq colors and fonts (#3)
    * added font styles
    
    * fixed proper bg collor of footer

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,19 +1,43 @@
 /**
  * plugins/vuetify.ts
  *
- * Framework documentation: https://vuetifyjs.com`
+ * Framework documentation: https://vuetifyjs.com
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from "vuetify";
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+// Точная палитра из твоего макета Figma
+const shelterLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    
+    background: "#FAF9F6", 
+    surface: "#FFFFFF",  
+    
+    primary: "#009EE1",    
+    secondary: "#EA7423",  
+   
+    "main-black": "#04080A", 
+    "main-grey": "#4B4B4B",  
+    "border-grey": "#CACACA", 
+
+    error: "#FF5252",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "shelterLightTheme",
+    themes: {
+      shelterLightTheme,
+    },
   },
-})
+});
