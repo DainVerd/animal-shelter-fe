3d47fc7abeced79824d83cb1b659e72f2ddd96fb
DainVerd <zobs10@inbox.lv>
2026-06-05 14:53:22 +0300

    first version for small sreens block 1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <v-container class="">
-    <v-row
-      justify="center"
-    >
+  <v-container>
+    <!-- animal shelter block -->
+    <v-row>
+      <!-- ─── left col ─────────────────────────── -->
       <v-col
         cols="3"
-        md="6"
-        class="d-flex justify-center mt-md-0"
+        sm="6"
+        class="d-flex flex-column"
+        style="gap: 16px"
       >
         <v-btn
           color="primary"
@@ -14,12 +15,21 @@
         >
           Lund
         </v-btn>
+
+        <!-- Видно только на sm+ (скрыто на xs) -->
+        <h2 class="text-h2 d-none d-sm-block">
+          Animal Shelter
+        </h2>
+        <p class="text-body-1 d-none d-sm-block">
+          to find the pets loving and reliable home
+        </p>
       </v-col>
 
+      <!-- ─── pic ──────────────────────────────── -->
       <v-col
         cols="9"
-        md="6"
-        class="d-flex justify-end mt-md-0"
+        sm="6"
+        class="d-flex justify-end"
       >
         <v-img
           src="https://placehold.co/200x250"
@@ -29,35 +39,36 @@
           cover
         />
       </v-col>
-      <v-col cols="12">
-        <v-row>
-          <v-col><h2 class="text-h2">Animal Shelter</h2></v-col>
-          <v-col><p>to find the pets loving and reliable home</p></v-col>
-        </v-row>
+
+      <!-- ─── only seen on  xs (<600px) ──────────── -->
+      <v-col
+        cols="6"
+        class="d-block d-sm-none"
+      >
+        <h2 class="text-h2">Animal Shelter</h2>
       </v-col>
-    </v-row>
-    <!-- btns block -->
-    <v-row>
-      <v-col cols="6">
-        <v-btn>
-          Find a Cat
-        </v-btn>
+      <v-col
+        cols="6"
+        class="d-block d-sm-none"
+      >
+        <p class="text-body-1">
+          to find the pets loving and reliable home
+        </p>
       </v-col>
-      <v-col cols="6">
-        <v-btn>
-          Find a Dog
-        </v-btn>
-      </v-col>
+
     </v-row>
 
     <!-- how many pets already at home block -->
      <v-row>
-      <v-col cols="12">
-        <h3 class="text-h3">123</h3>
+      <v-col
+        cols="12"
+        align="center"
+      >
+        <h2 class="text-h2">123</h2>
         <p class="text-label">pets are already at home</p>
       </v-col>
       <v-col cols="12">
-        <p class="text-label">
+        <p class="text-body-2">
           If you're considering adding a pet to your family, remember that adoption not only transforms the life of a shelter animal but can also be a deeply fulfilling and life-enriching experience for you.
         </p>
       </v-col>
@@ -65,8 +76,8 @@
 
      <!-- pets selectable row -->
       <v-row>
-        <v-col>
-          <h2>56 Pets available for adoption</h2>
+        <v-col align="center">
+          <h3 class="text-h3">56 Pets available for adoption</h3>
         </v-col>
         <v-col cols="12">
           <p>cards component</p>
@@ -86,9 +97,22 @@
 
       <!-- Join Us block -->
        <v-row>
-        <v-col cols="12">Logo</v-col>
+        <v-col
+          cols="12"
+          align="center"
+        >
+          <v-btn
+            prepend-icon="mdi-facebook"
+            stacked
+            elevation="0"
+            variant="text"
+            color="primary"
+          >
+            Join Us
+          </v-btn>
+        </v-col>
         <v-col cols="12">
-          <p>
+          <p class="text-body-1">
             Our Facebook group PetsPets  brings together adopted pet owners, volonteers, sponsors and animal walfare organization.
           </p>
         </v-col>
@@ -96,13 +120,16 @@
 
        <!-- adaptation stories block -->
         <v-row>
-          <v-col>
+          <v-col
+            cols="12" 
+            align="center"
+          >
             <h3 class="text-h3">
               Happy adoption stories
             </h3>
           </v-col>
-          <v-col>
-            image roundbin here
+          <v-col cols="12">
+            image roundbin here stub
           </v-col>
           <v-col cols="12">
             <v-btn
@@ -120,16 +147,19 @@
         <!--How do block-->
         <v-row>
           <v-col cols="12">
-            <h4 class="text-h4">How to I take a pet home from the shelter?</h4>
+            <h3 class="text-h3">How to I take a pet home from the shelter?</h3>
           </v-col>
           <v-col cols="12">
-            Search menu
+            Search menu stub
           </v-col>
         </v-row>
 
         <!--Visit us block-->
         <v-row>
-          <v-col cols="12">
+          <v-col
+            cols="12"
+            align="center"
+          >
             <h4 class="text-h4">Visit us</h4>
           </v-col>
           <v-col cols="12">
@@ -138,10 +168,10 @@
             </p>
           </v-col>
           <v-col cols="12">
-            LEAF MAP with coords
+            LEAF MAP with coords stub
           </v-col>
           <v-col cols="12">
-            <p>
+            <p class="text-body-2">
               There are still many friends waiting for their chance to bring love and joy into the lives of caring individuals and families. 
 
             </p>
