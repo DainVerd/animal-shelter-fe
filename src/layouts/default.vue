3ee9af9aaef55ae36cb4b37fb521b4ac100bc142
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-06-15 18:11:14 +0300

    feat: fix all linting and warning issues of the project (#18)
    * fixed linting issues
    
    * fixed linting issues
    
    * added user ts
    
    * added vite env

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -19,38 +19,115 @@
 
         <div class="d-none d-md-flex align-center gap-1">
           <template v-if="!authStore.isAuthenticated">
-            <v-btn variant="text" to="/about-us" class="text-none">About Us</v-btn>
-            <v-btn variant="text" to="/help-us" class="text-none">Help Us</v-btn>
-            <v-btn variant="text" to="/find-a-pet" class="text-none">Find a Pet</v-btn>
-            <v-btn variant="text" to="/favorites" class="text-none">Favorites</v-btn>
+            <v-btn
+              variant="text"
+              to="/about-us"
+              class="text-none"
+            >About Us</v-btn>
+            <v-btn
+              variant="text"
+              to="/help-us"
+              class="text-none"
+            >Help Us</v-btn>
+            <v-btn
+              variant="text"
+              to="/find-a-pet"
+              class="text-none"
+            >Find a Pet</v-btn>
+            <v-btn
+              variant="text"
+              to="/favorites"
+              class="text-none"
+            >Favorites</v-btn>
             
-            <v-btn color="primary" variant="elevated" rounded="lg" to="/auth/sign-in" class="text-none ml-4">Sign In</v-btn>
-            <v-btn color="secondary" variant="elevated" rounded="lg" to="/auth/sign-up" class="text-none ml-4">Sign Up</v-btn>
+            <v-btn
+              color="primary"
+              variant="elevated"
+              rounded="lg"
+              to="/auth/sign-in"
+              class="text-none ml-4"
+            >Sign In</v-btn>
+            <v-btn
+              color="secondary"
+              variant="elevated"
+              rounded="lg"
+              to="/auth/sign-up"
+              class="text-none ml-4"
+            >Sign Up</v-btn>
           </template>
 
           <template v-else>
-            <v-chip color="white" variant="elevated" size="small" class="mr-4 font-weight-bold text-primary">
+            <v-chip
+              color="white"
+              variant="elevated"
+              size="small"
+              class="mr-4 font-weight-bold text-primary">
               {{ authStore.user?.activeRole || 'Admin' }}
             </v-chip>
-            <v-btn color="white" variant="outlined" rounded="lg" @click="handleLogout" class="text-none">Sign Out</v-btn>
+            <v-btn
+              color="white"
+              variant="outlined"
+              rounded="lg"
+              @click="handleLogout"
+              class="text-none"
+            >Sign Out</v-btn>
           </template>
         </div>
 
-        <v-btn v-if="!authStore.isAuthenticated" icon variant="text" class="d-md-none text-grey-darken-3" @click="isMenuOpen = !isMenuOpen">
+        <v-btn
+          v-if="!authStore.isAuthenticated"
+          icon
+          variant="text"
+          class="d-md-none text-grey-darken-3"
+          @click="isMenuOpen = !isMenuOpen"
+        >
           <v-icon size="32">{{ isMenuOpen ? 'mdi-close' : 'mdi-menu' }}</v-icon>
         </v-btn>
       </v-container>
     </v-app-bar>
 
     <v-expand-transition>
-      <div v-if="isMenuOpen && !authStore.isAuthenticated" class="mobile-menu-overlay d-md-none bg-white">
+      <div
+        v-if="isMenuOpen && !authStore.isAuthenticated"
+        class="mobile-menu-overlay d-md-none bg-white"
+      >
         <v-container class="d-flex flex-column h-100 pa-6">
           <div class="d-flex flex-column gap-3 w-100 mt-4">
-            <v-btn variant="outlined" rounded="lg" block class="custom-menu-btn text-none justify-start text-grey-darken-4" @click="closeMenuAndNavigate('/about-us')">About Us</v-btn>
-            <v-btn variant="outlined" rounded="lg" block class="custom-menu-btn text-none justify-start text-grey-darken-4" @click="closeMenuAndNavigate('/help-us')">Help Us</v-btn>
-            <v-btn variant="outlined" rounded="lg" block class="custom-menu-btn text-none justify-start text-grey-darken-4" @click="closeMenuAndNavigate('/find-a-pet')">Find a Pet</v-btn>
-            <v-btn variant="outlined" rounded="lg" block class="custom-menu-btn text-none justify-start text-grey-darken-4" @click="closeMenuAndNavigate('/favorites')">Favorites</v-btn>
-            <v-btn variant="outlined" rounded="lg" block class="custom-menu-btn text-none justify-start text-grey-darken-4" @click="closeMenuAndNavigate('/auth/sign-in')">Log In</v-btn>
+            <v-btn
+              variant="outlined"
+              rounded="lg"
+              block
+              class="custom-menu-btn text-none justify-start text-grey-darken-4"
+              @click="closeMenuAndNavigate(`/about-us`)"
+            >About Us</v-btn>
+            <v-btn
+              variant="outlined"
+              rounded="lg"
+              block
+              class="custom-menu-btn text-none justify-start text-grey-darken-4"
+              @click="closeMenuAndNavigate(`/help-us`)"
+            >Help Us</v-btn>
+            <v-btn
+              variant="outlined"
+              rounded="lg"
+              block
+              class="custom-menu-btn text-none justify-start text-grey-darken-4"
+              @click="closeMenuAndNavigate(`/find-a-pet`)"
+            >Find a Pet</v-btn>
+            <v-btn
+              variant="outlined"
+              rounded="lg"
+              block
+              class="custom-menu-btn text-none justify-start text-grey-darken-4"
+              @click="closeMenuAndNavigate(`/favorites`)"
+            >Favorites</v-btn>
+            <v-btn
+              variant="outlined"
+              rounded="lg"
+              block
+              class="custom-menu-btn text-none justify-start text-grey-darken-4"
+              @click="closeMenuAndNavigate(`/auth/sign-in`)"
+            >Log In</v-btn>
           </div>
         </v-container>
       </div>
@@ -63,7 +140,9 @@
       permanent
       elevation="1"
     >
-      <div class="d-flex align-center pa-2 border-b" :class="isRail ? 'justify-center' : 'justify-end'">
+      <div
+        class="d-flex align-center pa-2 border-b"
+        :class="isRail ? 'justify-center' : 'justify-end'">
         <v-btn
           :icon="isRail ? 'mdi-chevron-right' : 'mdi-chevron-left'"
           variant="text"
@@ -72,14 +151,36 @@
         ></v-btn>
       </div>
 
-      <v-list density="compact" nav class="mt-2">
-        <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" to="/" color="primary"></v-list-item>
-        <v-list-item prepend-icon="mdi-paw" title="Animals" to="/animals" color="primary"></v-list-item>
-        <v-list-item prepend-icon="mdi-account-group" title="Users" to="/users" color="primary"></v-list-item>
+      <v-list
+        density="compact"
+        nav
+        class="mt-2"
+      >
+        <v-list-item
+          prepend-icon="mdi-view-dashboard"
+          title="Dashboard"
+          to="/"
+          color="primary"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-paw"
+          title="Animals" 
+          to="/animals"
+          color="primary"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-group"
+          title="Users"
+          to="/users"
+          color="primary"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-main class="bg-grey-lighten-4 d-flex flex-column" style="min-height: 100vh;">
+    <v-main
+      class="bg-grey-lighten-4 d-flex flex-column"
+      style="min-height: 100vh;"
+    >
       <div class="flex-grow-1">
         <router-view />
       </div>
@@ -98,9 +199,8 @@
 
   const isMenuOpen = ref(false);
   
-  // Состояния для Твич-сайдбара
-  const isSidebarOpen = ref(true); // Всегда true, чтобы сайдбар не исчезал в ноль
-  const isRail = ref(false);       // Переключатель узкого/широкого режима (по умолчанию широкий)
+  const isSidebarOpen = ref(true); 
+  const isRail = ref(false);      
 
   const closeMenuAndNavigate = (path: string) => {
     isMenuOpen.value = false;
@@ -110,6 +210,6 @@
   const handleLogout = async () => {
     isMenuOpen.value = false;
     await authStore.logout();
-    router.push('/auth/sign-in');
+    router.push(`/auth/sign-in`);
   };
 </script>
