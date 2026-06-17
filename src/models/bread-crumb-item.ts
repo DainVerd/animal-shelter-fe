a7f3781a3ea1b978a91d33c254fa2297605b7168
DainVerd <zobs10@inbox.lv>
2026-06-17 13:12:30 +0300

    created Page Wrapper to all my pages

---
+++ src/models/bread-crumb-item.ts
@@ -0,0 +1,6 @@
+export interface BreadcrumbItem {
+  title: string;
+  disabled?: boolean;
+  to?: string;
+  href?: string;
+}
---
+++ src/pages/find-a-pet.vue
@@ -0,0 +1,19 @@
+
+ <template>
+  <PageWrapper
+    title="Find a Pet"
+    :breadcrumbs="pageBreadcrumbs"
+    :withCard="false"
+  >
+  </PageWrapper>
+</template>
+
+<script setup lang="ts">
+import PageWrapper from "../components/PageWrapper.vue";
+import { BreadcrumbItem } from "../models/bread-crumb-item.ts";
+
+const pageBreadcrumbs: BreadcrumbItem[] = [
+  { title: "Home", to: "/" },
+  { title: "Find a Pet", to: "/find-a-pet" },
+];
+</script>
