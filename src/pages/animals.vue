ffda11cd077cf59236af6a240679eb9627277cb5
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-06-24 17:43:07 +0300

    feat: animals page v1 (#44)

---
+++ src/pages/animals.vue
@@ -0,0 +1,25 @@
+<route lang="json">
+{
+  "meta": {
+    "requiresAuth": true,
+    "allowedRoles": ["SuperAdmin", "Admin", "ShelterWorker"]
+  }
+}
+</route>
+
+<template>
+  <PageWrapper
+    title="Animals"
+    :breadcrumbs="pageBreadcrumbs"
+    withCard
+  >
+  </PageWrapper>
+</template>
+
+<script setup lang="ts">
+  import { BreadcrumbItem } from "../models/bread-crumb-item";
+
+  const pageBreadcrumbs: BreadcrumbItem[] = [
+  { title: "Animals", to: "/animals" }
+];
+</script>
