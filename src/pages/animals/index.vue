bd682949edcad76d14515b3e6083fa5d07c4a8b1
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-07-01 12:14:01 +0300

    feat: dialog for deleting specific animal from the table (#51)

--- src/pages/animals/index.vue
+++ src/pages/animals/index.vue
@@ -95,22 +95,58 @@
       </v-data-table-server>
     </v-card>
   </v-container>
+  <!-- dialog window for deleting specific animal -->
+  <v-dialog
+    max-width="500"
+    v-model="openDialog"
+  >
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-bind="activatorProps"
+        color="surface-variant"
+        text="Open Dialog"
+        variant="flat"
+      ></v-btn>
+    </template>
 
+    <template v-slot:default="{ isActive }">
+      <v-card title="Delete Animal?">
+        <v-card-text>
+          Are you really want to delete this animal?
+        </v-card-text>
+
+        <v-card-actions class="text-align-center">
+          <v-spacer></v-spacer>
+          <v-btn
+            text="Yes"
+            @click="deleteAnimal()"
+          ></v-btn>
+          <v-btn
+            text="Close Dialog"
+            @click="isActive.value = false"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
   </PageWrapper>
 </template>
 
 <script setup lang="ts">
 import { BreadcrumbItem } from "../../models/bread-crumb-item";
-import { ref } from "vue";
+import { ref, onUnmounted } from "vue";
 import { animalService } from "../../services/animal-service";
 import Animal from "../../models/animal";
 import { formatDate } from "../../utils/date-formatter";
 import { useRouter } from "vue-router";
 import Gender from "../../enums/gender";
 import { formatGenderName } from "../../utils/gender-formatter";
+import { useNotificationStore } from "../../stores/notification-store";
 
 
 const router = useRouter();
+const notification = useNotificationStore();
+const controller = new AbortController();
 const pageBreadcrumbs: BreadcrumbItem[] = [
   { title: "Animals", to: "/animals" }
 ];
@@ -119,6 +155,8 @@ const animals = ref<Animal[]>([]);
 const totalCount = ref(0);
 const loading = ref(false);
 const itemsPerPage = ref(10);
+const openDialog = ref(false);
+const selectedAnimalId = ref<number | null>(null);
 
 const headers = [
   { title: "Name", key: "name" },
@@ -149,8 +187,37 @@ const loadData = async ({ page, itemsPerPage: size }: any) => {
   }
 };
 
-const editAnimal = (item: any) => {
+const editAnimal = (item: Animal) => {
   router.push(`/animals/upsert/${item.id}`);
+};
+
+const confirmDelete = (item: Animal) => {
+  openDialog.value = true;
+  selectedAnimalId.value = item.id;
+};
+
+const deleteAnimal = async () => {
+
+  console.log("animal to Delete", selectedAnimalId.value);
+  
+  if(!selectedAnimalId.value)
+    return;
+
+  try {
+    await animalService.deleteAnimal(selectedAnimalId.value, controller.signal);
+    notification.notify("Deleted animal!"); 
+     animals.value = animals.value.filter(animal => animal.id !== selectedAnimalId.value);
+      totalCount.value = totalCount.value - 1;
+  } catch(err){
+    notification.notify("Error to delete animal try again.", "error");
+  } finally {
+    selectedAnimalId.value = null;
+    openDialog.value = false;
+  }
+
+  onUnmounted(() => {
+  controller.abort();
+});
 };
 
 </script>
